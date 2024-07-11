f4e716a67b407de114248f1754750644ee4440b6
popup0221 <popup0221@naver.com>
2024-07-11 23:59:19 +0900

    first commit

---
+++ src/main/java/dig/into/spring/DigIntoSpringApplication.java
@@ -0,0 +1,13 @@
+package dig.into.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigIntoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigIntoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dig/into/spring/DigIntoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package dig.into.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigIntoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
